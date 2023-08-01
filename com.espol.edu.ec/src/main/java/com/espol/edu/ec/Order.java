533947fb933c27a57c80327cb6ef8793a4706d12
Viviana Muñoz <74017976+ivi-bot@users.noreply.github.com>
2023-07-31 20:36:54 -0500

    maven project with cucumber and 5 acceptance test

---
+++ com.espol.edu.ec/src/main/java/com/espol/edu/ec/Order.java
@@ -0,0 +1,19 @@
+package com.espol.edu.ec;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Order {
+    private final Map<Meal, Integer> selectedMeals;
+
+    public Order() {
+        this.selectedMeals = new HashMap<>();
+    }
+
+    public void addMeal(Meal meal, int quantity) {
+        selectedMeals.put(meal, quantity);
+    }
+
+    public Map<Meal, Integer> getSelectedMeals() {
+        return selectedMeals;
+    }
+}
